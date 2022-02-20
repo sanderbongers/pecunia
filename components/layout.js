530a41cb2e4eb5db553aaaf25b6f03ca8b950f28
Sander Bongers <sander@bonge.rs>
2022-02-20 16:14:35 +0100

    Created Layout component

--- components/layout.js
+++ components/layout.js
@@ -1,17 +1,20 @@
+import Head from 'next/head'
+import DropZone from '../components/drop-zone'
+
 export default function Layout({ children }) {
   return (
     <>
-      <div className={'absolute w-full h-full top-0 left-0 z-50 bg-neutral-900 text-white text-2xl font-semibold transition-opacity duration-400' + (!isVisible ? ' invisible opacity-0' : '')}>
-        <div className="w-[calc(100vw-10rem)] h-[calc(100vh-10rem)] mt-20 ml-20 rounded-3xl border-dashed border-4 border-neutral-600">
-          <div className="flex justify-center items-center h-full">
-            <div className={'inline-flex items-center transition-transform duration-400' + (!isVisible ? ' scale-75' : '')}>
-              <DocumentDownloadIcon className="inline-block h-8 w-8 mr-1" />
-              <span className="uppercase">Drop .csv</span>
-            </div>
-          </div>
+      <DropZone>
+        <div className="min-h-screen bg-gradient-to-r from-yellow-300 to-yellow-400 px-8">
+          <Head>
+            <title>Pecunia</title>
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
+          </Head>
+
+          <main>{children}</main>
         </div>
-      </div>
-      {children}
+      </DropZone>
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,7 @@
-import Head from 'next/head'
-import DropZone from '../components/drop-zone'
+import Layout from '../components/layout'
 
 export default function App() {
   return (
-    <DropZone>
-      <div className="min-h-screen bg-gradient-to-r from-yellow-300 to-yellow-400 px-8">
-        <Head>
-          <title>Pecunia</title>
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
-        </Head>
-
-        <main className="flex flex-1 flex-col justify-center items-center py-16">
-          <h1 className="text-6xl font-bold mb-8">Pecunia</h1>
-        </main>
-      </div>
-    </DropZone>
+    <Layout></Layout>
   )
 }
